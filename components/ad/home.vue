d90efc8b4f3694dda825bddbbdb2fd5e55ab6305
mendmania <mendmania@gmail.com>
2024-03-06 23:32:18 +0100

    [added] ad component

---
+++ components/ad/home.vue
@@ -0,0 +1,23 @@
+<script setup>
+onMounted(() => {
+    // Load the AdSense Script
+    // const script = document.createElement('script');
+    // script.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1757918464869662";
+    // script.async = true;
+    // script.crossOrigin = 'anonymous';
+    // document.head.appendChild(script);
+
+    // // Initialize the ad after the script is loaded
+    // script.onload = () => {
+    //     (window.adsbygoogle = window.adsbygoogle || []).push({});
+    // };
+})
+</script>
+
+<template>
+    <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-1757918464869662" data-ad-slot="8306173260"
+        data-ad-format="auto" data-full-width-responsive="true"></ins>
+
+</template>
+
+<style></style>
--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,7 @@
 
 <template>
   <div>
+    <!-- <AdHome /> -->
     <TypeInput />
   </div>
 </template>
